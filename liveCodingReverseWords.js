1f99e408ef2992626bf9b668fa289f25b3b97491
Gabriel Gouvêa <gouswat@gmail.com>
2025-07-26 00:27:50 -0300

    Solving a cool problem

---
+++ liveCodingReverseWords.js
@@ -0,0 +1,33 @@
+// Write a function to revert every word in a string. For example:
+// “the rabbit jumps” → “eht tibbar spmuj”
+
+function reverseWords(phrase){
+    const words = phrase.split(" ");
+
+    const reversedWords = words.map(word => {
+        const preservedParts = word.split(".");
+        const reversedWord = preservedParts.map(part => part.split("").reverse().join(""));
+
+        const finalWord = reversedWord.join(".");
+
+        return finalWord;
+    });
+
+    return reversedWords.join(" ");
+}
+
+const testReverseWords = (string, expected) => {
+    if(reverseWords(string) === expected){
+        console.log(true);
+        return;
+    }
+
+    console.log(false);
+}
+
+testReverseWords("hello", "olleh");
+testReverseWords("the rabbit jumps", "eht tibbar spmuj");
+testReverseWords("the  rabbit  jumps", "eht  tibbar  spmuj");
+testReverseWords(" the  rabbit  jumps ", " eht  tibbar  spmuj ");
+testReverseWords("the rabbit can't jump", "eht tibbar t'nac pmuj");
+testReverseWords("the rabbit. jump", "eht tibbar. pmuj");
